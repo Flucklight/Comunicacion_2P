fe452c22b455076c693eb1458f1111dd4b0fa416
Victor Manuel Sanchez Sanchez <vicool275@gmail.com>
2019-05-12 01:28:01 -0500

    Se actualizo el proyecto para manejar la Seccion Critica

--- src/utility/Escucha.java
+++ src/utility/Escucha.java
@@ -7,7 +7,11 @@ public class Escucha extends JFrame{
     private JPanel panel;
     private JLabel gif;
     private PipedInputStream pis;
+    private Estados estado;
     private File miLibro;
+    private File direccionDeMemoria;
+    private BufferedReader bandera;
+    private BufferedWriter cambioEstado;
     private BufferedWriter escritor;
     private String entrada;
     private Runnable escucharUnCuento;
@@ -16,11 +20,14 @@ public class Escucha extends JFrame{
         super("Escucha");
         setLocationRelativeTo(this);
         setContentPane(panel);
-        setSize(360, 240);
+        setSize(320, 234);
         setResizable(false);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         entrada = "";
         pis = new PipedInputStream(pos);
+        direccionDeMemoria = new File(System.getProperty("user.dir") + "/src/items/5000.mem");
+        bandera = new BufferedReader(new FileReader(direccionDeMemoria));
+        cambioEstado = new BufferedWriter(new FileWriter(direccionDeMemoria));
         miLibro = new File(System.getProperty("user.dir") + "/src/out/miLibro.txt");
         escucharUnCuento = () -> escucharElCuento(pis);
     }
@@ -30,8 +37,18 @@ public class Escucha extends JFrame{
             int verso = -1;
             this.escritor = new BufferedWriter(new FileWriter(this.miLibro));
             while ((verso = pis.read()) != -1) {
-                this.entrada = Character.toString(((char) verso));
-                this.escritor.write(entrada);
+                switch (estado){
+                    case ESPERA:
+                        manejoDeSeccionCritica();
+                        break;
+                    case LECTURA:
+                        this.entrada = Character.toString(((char) verso));
+                        this.escritor.write(entrada);
+                        this.cambioEstado.write(0);
+                        this.cambioEstado.close();
+                        this.estado = Estados.ESPERA;
+                        break;
+                }
             }
             this.setVisible(true);
             this.escritor.close();
@@ -41,7 +58,20 @@ public class Escucha extends JFrame{
         }
     }
 
+    public void manejoDeSeccionCritica() {
+        try {
+            String s = bandera.readLine();
+            this.bandera.close();
+            if (s.equals("1")){
+                this.estado = Estados.LECTURA;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void start() {
+        this.estado = Estados.ESPERA;
         new Thread(this.escucharUnCuento).start();
     }
 
--- src/utility/Lector.java
+++ src/utility/Lector.java
@@ -11,19 +11,26 @@ public class Lector extends JFrame {
     private PipedOutputStream pos;
     private String elCuento = "";
     private char caracter = ' ';
+    private Estados estado;
     private File cuento;
+    private File direccionDeMemoria;
     private BufferedReader cargar;
+    private BufferedReader bandera;
+    private BufferedWriter cambioEstado;
     private Runnable contarUnCuneto;
 
     public Lector() throws IOException {
         super("Escritor");
-        setLocationRelativeTo(this);
         setContentPane(panel1);
         setSize(Toolkit.getDefaultToolkit().getScreenSize());
         setResizable(false);
+        setLocationRelativeTo(this);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         pos = new PipedOutputStream();
         cuento = new File(System.getProperty("user.dir") + "/src/items/cuento.txt");
+        direccionDeMemoria = new File(System.getProperty("user.dir") + "/src/items/5000.mem");
+        bandera = new BufferedReader(new FileReader(direccionDeMemoria));
+        cambioEstado = new BufferedWriter(new FileWriter(direccionDeMemoria));
         cargar = new BufferedReader(new FileReader(cuento));
         String s;
         while ((s = cargar.readLine()) != null) {
@@ -34,23 +41,47 @@ public class Lector extends JFrame {
 
     public void contarElCuento(PipedOutputStream pos) {
         try {
-            for (int i = 0; i < elCuento.length(); i++) {
-                caracter = elCuento.charAt(i);
-                this.Texto.append(Character.toString(caracter));
-                Thread.sleep(1);
-                pos.write(((byte) caracter));
-            }
-            pos.flush();
+            int i = 0;
+            do {
+                switch (estado) {
+                    case ESPERA:
+                        manejoDeSeccionCritica();
+                        break;
+                    case ESCRITURA:
+                        caracter = elCuento.charAt(i);
+                        i++;
+                        Texto.append(Character.toString(caracter));
+                        Thread.sleep(10);
+                        pos.write(((byte) caracter));
+                        cambioEstado.write(1);
+                        cambioEstado.close();
+                        estado = Estados.ESPERA;
+                        break;
+                }
+            }while (i < elCuento.length());
             System.out.println("Buenas noches");
-            pos.write("Buenas noches".getBytes());
+            pos.write(((byte) ';'));
             pos.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public void manejoDeSeccionCritica() {
+        try {
+            String s = bandera.readLine();
+            bandera.close();
+            if (s.equals("0")){
+                estado = Estados.ESCRITURA;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void start() {
         this.setVisible(true);
+        estado = Estados.ESPERA;
         new Thread(this.contarUnCuneto).start();
     }
 
